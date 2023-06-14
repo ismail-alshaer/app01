ede0256e35f44b6399f3c1d27e0375cb483bcebd
Ismail Alshaer <ismail.alshaer@gmail.com>
2023-06-14 04:18:29 +0200

    1st commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+//#[deny(warnings)]
+fn main() {
+    println!("Hello, world!");
+    let mut x = 5;    
+    let mut u_num: u8 = 0;
+
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+    u_num = u_num + 1;
+    println!("the result is: {u_num}");
+}
